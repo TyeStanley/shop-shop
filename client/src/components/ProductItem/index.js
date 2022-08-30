4b941af78611cf37cf1665c0a778321706298786
Tye Stanley <tyestanley090@gmail.com>
2022-08-29 21:41:33 -0700

    implemented stripe

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from '../../utils/GlobalState';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
+  const [state, dispatch] = useStoreContext();
+
   const {
     image,
     name,
@@ -14,16 +16,10 @@ function ProductItem(item) {
     quantity
   } = item;
 
-
-  const [state, dispatch] = useStoreContext();
-
-  const { cart } = state;
+  const { cart } = state
 
   const addToCart = () => {
-    // find the cart item with the matching id
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-  
-    // if there was a match, call UPDATE with a new purchase quantity
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
@@ -41,7 +37,7 @@ function ProductItem(item) {
       });
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
-  };
+  }
 
   return (
     <div className="card px-1 py-1">
